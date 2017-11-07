52869393831b161996ed1418a2e0d9ba1757cdc4
Kevin Kuhls <kekuhls@cisco.com>
2017-11-07 12:02:20 -0500

    CL LATAM

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 # Prepare environment for DEVNET2449 Lab
 
 vagrant up rtr1
-docker run -it --rm -p58888:58888 -v$(pwd):/home/docker/ kuhlskev/ansible_ydk_jupyter
+docker run -it --rm -p58889:58888 -v$(pwd):/home/docker/ kuhlskev/ansible_ydk_jupyter
